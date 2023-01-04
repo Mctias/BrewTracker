d1e73deb35c65caaef088ceb1c7f7adc8ec34460
Mattias Persson <mattias.persson@skf.com>
2023-01-04 20:22:36 +0100

    Add project files.

---
+++ BrewTracker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BrewTracker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BrewTracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BrewTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
